d986a3068905c5c464c9fdd113b9ad95589f9449
Muzamil Mohamed <mrm00163@gmail.com>
2025-06-09 14:36:32 +0300

    some edits

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,4 @@ Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [UserController::class, 'login'])->name('login.submit');
 
 // Logout route
-<<<<<<< HEAD
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-
-// Add these two new routes for registration
-// Route::get ('/register', [RegisterController::class, 'create'])->name('register');
-// Route::post('/register', [RegisterController::class, 'store']);
-=======
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
->>>>>>> d15e8bd702f1733b03ec4115d23da4dbbbfde93d
